80d72532a0c7d14b1d48cc228bb0b749a2678321
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-11-06 18:48:43 -0300

    feat: export Logger class alongside existing data structures

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ import DoublyLinkedList from "./dataStructures/DoublyLinkedList";
 import Queue from "./dataStructures/Queue";
 import SingleLinkedList from "./dataStructures/SingleLinkedList";
 import Stack from "./dataStructures/Stack";
+import Logger from "./lib/Logger";
 
-export { SingleLinkedList, Stack, Queue, DoublyLinkedList as LinkedList };
+export {
+  SingleLinkedList,
+  Stack,
+  Queue,
+  DoublyLinkedList as LinkedList,
+  Logger,
+};
