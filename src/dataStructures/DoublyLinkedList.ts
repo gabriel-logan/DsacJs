b6b8fd7db7996ef3a209c445f53038e70f80bf60
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-11-07 00:36:33 -0300

    feat: implement add, removeAt, removeFirst, removeLast, contains, clear, peek, get, getFirst, getLast, indexOf, iterator, toArray, and poll methods in DoublyLinkedList

--- src/dataStructures/DoublyLinkedList.ts
+++ src/dataStructures/DoublyLinkedList.ts
@@ -98,4 +98,246 @@ export default class DoublyLinkedList<T = any> {
 
     this.logger.info(listValues);
   }
+
+  add(value: T): void;
+  add(index: number, value: T): void;
+
+  // O(n) - O(1)
+  add(indexOrValue: number | T, value?: T): void {
+    if (typeof indexOrValue === "number" && value) {
+      this.addAtIndex(indexOrValue, value);
+    } else {
+      this.addLast(indexOrValue as T);
+    }
+  }
+
+  // O(n)
+  private addAtIndex(index: number, value: T): void {
+    if (index < 0 || index > this.size) {
+      throw new Error("Index out of bounds");
+    }
+
+    if (index === 0) {
+      this.addFirst(value);
+      return;
+    }
+
+    if (index === this.size) {
+      this.addLast(value);
+      return;
+    }
+
+    let current = this.head;
+    let i = 0;
+
+    while (current && i < index) {
+      current = current.next;
+      i++;
+    }
+
+    if (current) {
+      const newNode = new ListNode(value);
+      newNode.next = current;
+      newNode.prev = current.prev;
+      if (current.prev) {
+        current.prev.next = newNode;
+      }
+      current.prev = newNode;
+      this.size++;
+    }
+  }
+
+  // O(n)
+  removeAt(index: number): T | null {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    if (index === 0) {
+      return this.removeFirst();
+    }
+
+    if (index === this.size - 1) {
+      return this.removeLast();
+    }
+
+    let current = this.head;
+
+    let i = 0;
+
+    while (current && i < index) {
+      current = current.next;
+      i++;
+    }
+
+    if (current) {
+      if (current.prev) {
+        current.prev.next = current.next;
+      }
+      if (current.next) {
+        current.next.prev = current.prev;
+      }
+
+      this.size--;
+
+      return current.value;
+    }
+
+    return null;
+  }
+
+  // O(1)
+  removeFirst(): T | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const value = this.head?.value ?? null;
+
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else if (this.head) {
+      this.head = this.head.next;
+      if (this.head) {
+        this.head.prev = null;
+      }
+    }
+
+    this.size--;
+
+    return value;
+  }
+
+  // O(1)
+  removeLast(): T | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const value = this.tail?.value ?? null;
+
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else if (this.tail) {
+      this.tail = this.tail.prev;
+      if (this.tail) {
+        this.tail.next = null;
+      }
+    }
+
+    this.size--;
+
+    return value;
+  }
+
+  // O(n)
+  contains(value: T): boolean {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+
+      current = current.next;
+    }
+
+    return false;
+  }
+
+  // O(1)
+  clear(): void {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  // O(1)
+  peek(): T | null {
+    return this.head?.value ?? null;
+  }
+
+  // O(n)
+  get(index: number): T | null {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    let current = this.head;
+    let i = 0;
+
+    while (current && i < index) {
+      current = current.next;
+      i++;
+    }
+
+    return current?.value ?? null;
+  }
+
+  // O(1)
+  getFirst(): T | null {
+    return this.head?.value ?? null;
+  }
+
+  // O(1)
+  getLast(): T | null {
+    return this.tail?.value ?? null;
+  }
+
+  // O(n)
+  indexOf(value: T): number {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === value) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  // O(n)
+  iterator(): IterableIterator<T> {
+    let current = this.head;
+
+    return {
+      next(): IteratorResult<T> {
+        if (current) {
+          const value = current.value;
+          current = current.next;
+          return { done: false, value };
+        }
+
+        return { done: true, value: null };
+      },
+      [Symbol.iterator](): IterableIterator<T> {
+        return this;
+      },
+    };
+  }
+
+  // O(n)
+  toArray(): T[] {
+    const listValues = [];
+    let current = this.head;
+
+    while (current) {
+      listValues.push(current.value);
+      current = current.next;
+    }
+
+    return listValues;
+  }
+
+  // O(n)
+  poll(): T | null {
+    return this.removeFirst();
+  }
 }
--- tests/dataStructures/DoublyLinkedList.test.ts
+++ tests/dataStructures/DoublyLinkedList.test.ts
@@ -18,6 +18,7 @@ describe("DoublyLinkedList", () => {
       expect(list.size).toBe(1);
       expect(list.head?.value).toBe(1);
       expect(list.tail?.value).toBe(1);
+
       expect(list.head?.next).toBeNull();
       expect(list.head?.prev).toBeNull();
       expect(list.tail?.next).toBeNull();
@@ -46,6 +47,7 @@ describe("DoublyLinkedList", () => {
       expect(list.size).toBe(1);
       expect(list.head?.value).toBe(1);
       expect(list.tail?.value).toBe(1);
+
       expect(list.head?.next).toBeNull();
       expect(list.head?.prev).toBeNull();
       expect(list.tail?.next).toBeNull();
@@ -60,6 +62,7 @@ describe("DoublyLinkedList", () => {
       expect(list.size).toBe(3);
       expect(list.head?.value).toBe(1);
       expect(list.tail?.value).toBe(3);
+
       expect(list.head?.next?.value).toBe(2);
       expect(list.head?.prev).toBeNull();
       expect(list.tail?.next).toBeNull();
@@ -125,36 +128,458 @@ describe("DoublyLinkedList", () => {
     });
   });
 
-  describe("add At", () => {});
-  describe("add", () => {});
+  describe("add At", () => {
+    it("should add a new node at a given index", () => {
+      const list = new DoublyLinkedList();
+      list.add(0, 1);
+      expect(list.size).toBe(1);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(1);
 
-  describe("Remove At", () => {});
+      expect(list.head?.next).toBeNull();
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev).toBeNull();
+    });
 
-  describe("Remove", () => {});
+    it("should add a new node at the beginning of the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(0, 1);
+      list.add(0, 2);
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(2);
+      expect(list.tail?.value).toBe(1);
 
-  describe("RemoveFirst", () => {});
+      expect(list.head?.next?.value).toBe(1);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
 
-  describe("RemoveLast", () => {});
+    it("should add a new node at the end of the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(0, 1);
+      list.add(1, 2);
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(2);
 
-  describe("Contains", () => {});
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(1);
+    });
 
-  describe("Sort", () => {});
+    it("should add a new node at a given index", () => {
+      const list = new DoublyLinkedList();
+      list.add(0, 1);
+      list.add(1, 2);
+      list.add(1, 3);
+      expect(list.size).toBe(3);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(2);
 
-  describe("Clear", () => {});
+      expect(list.head?.next?.value).toBe(3);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(3);
+    });
 
-  describe("Peek", () => {});
+    it("should throw an error if the index is out of bounds", () => {
+      const list = new DoublyLinkedList();
+      expect(() => list.add(1, 1)).toThrow("Index out of bounds");
+    });
+  });
+  describe("add", () => {
+    it("should add a new node at the end of the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      expect(list.size).toBe(1);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(1);
 
-  describe("Get", () => {});
+      expect(list.head?.next).toBeNull();
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev).toBeNull();
+    });
 
-  describe("IndexOf", () => {});
+    it("should add a new node at the end of the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(2);
 
-  describe("Iterator", () => {});
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(1);
+    });
+  });
 
-  describe("ToArray", () => {});
+  describe("Remove At", () => {
+    it("should removeAt a node at a given index", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.removeAt(0);
+      expect(list.size).toBe(0);
+      expect(list.head).toBeNull();
+      expect(list.tail).toBeNull();
 
-  describe("Poll", () => {});
+      expect(list.head?.next ?? null).toBeNull();
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev ?? null).toBeNull();
+    });
 
-  describe("GetFirst", () => {});
+    it("should removeAt a node at the beginning of the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.removeAt(0);
+      expect(list.size).toBe(1);
+      expect(list.head?.value).toBe(2);
+      expect(list.tail?.value).toBe(2);
 
-  describe("GetLast", () => {});
+      expect(list.head?.next).toBeNull();
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev).toBeNull();
+    });
+
+    it("should removeAt a node at the end of the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.removeAt(1);
+      expect(list.size).toBe(1);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(1);
+
+      expect(list.head?.next).toBeNull();
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev).toBeNull();
+    });
+
+    it("should removeAt a node at a given index", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.removeAt(1);
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(3);
+
+      expect(list.head?.next?.value).toBe(3);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(1);
+    });
+
+    it("should return null if the index is out of bounds", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      expect(list.removeAt(1)).toBeNull();
+
+      const list2 = new DoublyLinkedList();
+      expect(list2.removeAt(-1)).toBeNull();
+    });
+
+    it("should return null if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.removeAt(0)).toBeNull();
+
+      expect(list.head?.next ?? null).toBeNull();
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev ?? null).toBeNull();
+    });
+
+    it("should return null if current is null", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.removeAt(3);
+      expect(list.size).toBe(3);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(3);
+
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
+  });
+
+  describe("RemoveFirst", () => {
+    it("should remove the first node", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.removeFirst();
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(2);
+      expect(list.tail?.value).toBe(3);
+
+      expect(list.head?.next?.value).toBe(3);
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
+
+    it("should remove the first node", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.removeFirst();
+      expect(list.size).toBe(0);
+      expect(list.head).toBeNull();
+      expect(list.tail).toBeNull();
+
+      expect(list.head?.next ?? null).toBeNull();
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev ?? null).toBeNull();
+    });
+  });
+
+  describe("RemoveLast", () => {
+    it("should return null if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.removeLast()).toBeNull();
+    });
+
+    it("should remove the last node", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.removeLast();
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(2);
+
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev?.value).toBe(1);
+    });
+
+    it("should remove the last node", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.removeLast();
+      expect(list.size).toBe(0);
+      expect(list.head).toBeNull();
+      expect(list.tail).toBeNull();
+
+      expect(list.head?.next ?? null).toBeNull();
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev ?? null).toBeNull();
+    });
+  });
+
+  describe("Contains", () => {
+    it("should return true if the list contains the value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.contains(2)).toBe(true);
+    });
+
+    it("should return false if the list does not contain the value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.contains(4)).toBe(false);
+    });
+  });
+
+  describe("Clear", () => {
+    it("should clear the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.size).toBe(3);
+
+      list.clear();
+
+      expect(list.head).toBeNull();
+      expect(list.tail).toBeNull();
+
+      expect(list.head?.next ?? null).toBeNull();
+      expect(list.head?.prev ?? null).toBeNull();
+      expect(list.tail?.next ?? null).toBeNull();
+      expect(list.tail?.prev ?? null).toBeNull();
+    });
+  });
+
+  describe("Peek", () => {
+    it("should return the first value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.peek()).toBe(1);
+    });
+
+    it("should return null if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.peek()).toBeNull();
+    });
+  });
+
+  describe("Get", () => {
+    it("should return the value at a given index", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.get(1)).toBe(2);
+    });
+
+    it("should return null if the index is out of bounds", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.get(3)).toBeNull();
+    });
+  });
+
+  describe("GetFirst", () => {
+    it("should return the first value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.getFirst()).toBe(1);
+    });
+
+    it("should return null if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.getFirst()).toBeNull();
+    });
+  });
+
+  describe("GetLast", () => {
+    it("should return the last value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.getLast()).toBe(3);
+    });
+
+    it("should return null if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.getLast()).toBeNull();
+    });
+  });
+
+  describe("IndexOf", () => {
+    it("should return the index of the value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.indexOf(2)).toBe(1);
+    });
+
+    it("should return -1 if the value is not found", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.indexOf(4)).toBe(-1);
+    });
+  });
+
+  describe("Iterator", () => {
+    it("should iterate over the list", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+
+      const iterator = list.iterator();
+      expect(iterator.next().value).toBe(1);
+      expect(iterator.next().value).toBe(2);
+      expect(iterator.next().value).toBe(3);
+      expect(iterator.next().done).toBe(true);
+    });
+
+    it("should return done true if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      const iterator = list.iterator();
+      expect(iterator.next().done).toBe(true);
+    });
+
+    it("should be iterable with for...of loop", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+
+      const values = [];
+      for (const value of list.iterator()) {
+        values.push(value);
+      }
+
+      expect(values).toEqual([1, 2, 3]);
+    });
+  });
+
+  describe("ToArray", () => {
+    it("should return an array of values", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.toArray()).toEqual([1, 2, 3]);
+
+      expect(list.head?.value).toBe(1);
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.value).toBe(3);
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
+
+    it("should return an empty array if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.toArray()).toEqual([]);
+    });
+  });
+
+  describe("Poll", () => {
+    it("should remove and return the first value", () => {
+      const list = new DoublyLinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      expect(list.poll()).toBe(1);
+      expect(list.size).toBe(2);
+      expect(list.head?.value).toBe(2);
+
+      expect(list.head?.next?.value).toBe(3);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
+
+    it("should return null if the list is empty", () => {
+      const list = new DoublyLinkedList();
+      expect(list.poll()).toBeNull();
+    });
+  });
 });
