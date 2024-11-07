ca7f936310a51e299d8db3fa8716ffeb29120314
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-11-06 21:30:25 -0300

    feat: add insertionSort tests and implement addFirst, addLast, print, and printReverse methods in DoublyLinkedList; enhance related tests

--- src/dataStructures/DoublyLinkedList.ts
+++ src/dataStructures/DoublyLinkedList.ts
@@ -15,21 +15,87 @@ class ListNode<T = any> {
 export default class DoublyLinkedList<T = any> {
   private readonly logger = new Logger();
 
-  public readonly head: ListNode<T> | null = null;
-  public readonly tail: ListNode<T> | null = null;
+  public head: ListNode<T> | null;
+  public tail: ListNode<T> | null;
   public size: number = 0;
 
   constructor() {
-    this.logger.log(
-      "DoublyLinkedList created" +
-        this.head +
-        this.tail +
-        this.head?.next +
-        this.tail?.prev,
-    );
+    this.head = null;
+    this.tail = null;
   }
 
+  // O(1)
   isEmpty(): boolean {
     return this.size === 0;
+  }
+
+  // O(1)
+  addFirst(value: T): void {
+    const newNode = new ListNode(value);
+
+    if (this.isEmpty()) {
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      if (this.head) {
+        this.head.prev = newNode;
+      }
+    }
+
+    this.head = newNode;
+
+    this.size++;
+  }
+
+  // O(1)
+  addLast(value: T): void {
+    const newNode = new ListNode(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+    } else {
+      if (this.tail) {
+        this.tail.next = newNode;
+      }
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+
+    this.size++;
+  }
+
+  // O(n)
+  print(): void {
+    if (this.isEmpty()) {
+      return this.logger.info("List is empty");
+    }
+
+    const listValues = [];
+    let current = this.head;
+
+    while (current) {
+      listValues.push(current.value);
+      current = current.next;
+    }
+
+    this.logger.info(listValues);
+  }
+
+  // O(n)
+  printReverse(): void {
+    if (this.isEmpty()) {
+      return this.logger.info("List is empty");
+    }
+
+    const listValues = [];
+    let current = this.tail;
+
+    while (current) {
+      listValues.push(current.value);
+      current = current.prev;
+    }
+
+    this.logger.info(listValues);
   }
 }
--- tests/dataStructures/DoublyLinkedList.test.ts
+++ tests/dataStructures/DoublyLinkedList.test.ts
@@ -1,3 +1,4 @@
+import { Logger } from "../../src";
 import DoublyLinkedList from "../../src/dataStructures/DoublyLinkedList";
 
 describe("DoublyLinkedList", () => {
@@ -10,13 +11,119 @@ describe("DoublyLinkedList", () => {
     it("should return false if the list is not empty", () => {});
   });
 
-  describe("AddFirst", () => {});
+  describe("AddFirst", () => {
+    it("should add a new node to the beginning of the list", () => {
+      const list = new DoublyLinkedList();
+      list.addFirst(1);
+      expect(list.size).toBe(1);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(1);
+      expect(list.head?.next).toBeNull();
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev).toBeNull();
+    });
 
-  describe("AddLast", () => {});
+    it("should add multiple nodes to the beginning of the list", () => {
+      const list = new DoublyLinkedList();
+      list.addFirst(1);
+      list.addFirst(2);
+      list.addFirst(3);
+      expect(list.size).toBe(3);
+      expect(list.head?.value).toBe(3);
+      expect(list.tail?.value).toBe(1);
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
+  });
 
-  describe("Print", () => {});
+  describe("AddLast", () => {
+    it("should add a new node to the end of the list", () => {
+      const list = new DoublyLinkedList();
+      list.addLast(1);
+      expect(list.size).toBe(1);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(1);
+      expect(list.head?.next).toBeNull();
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev).toBeNull();
+    });
 
-  describe("PrintReverse", () => {});
+    it("should add multiple nodes to the end of the list", () => {
+      const list = new DoublyLinkedList();
+      list.addLast(1);
+      list.addLast(2);
+      list.addLast(3);
+      expect(list.size).toBe(3);
+      expect(list.head?.value).toBe(1);
+      expect(list.tail?.value).toBe(3);
+      expect(list.head?.next?.value).toBe(2);
+      expect(list.head?.prev).toBeNull();
+      expect(list.tail?.next).toBeNull();
+      expect(list.tail?.prev?.value).toBe(2);
+    });
+  });
+
+  describe("Print", () => {
+    it("should return an empty string if the list is empty", () => {
+      const list = new DoublyLinkedList();
+
+      const spyLogger = jest
+        .spyOn(Logger.prototype, "info")
+        .mockImplementation(() => {});
+
+      list.print();
+
+      expect(spyLogger).toHaveBeenCalledWith("List is empty");
+    });
+
+    it("should print the list", () => {
+      const list = new DoublyLinkedList();
+      list.addFirst(1);
+      list.addFirst(2);
+      list.addFirst(3);
+
+      const spyLogger = jest
+        .spyOn(Logger.prototype, "info")
+        .mockImplementation(() => {});
+
+      list.print();
+
+      expect(spyLogger).toHaveBeenCalledWith([3, 2, 1]);
+    });
+  });
+
+  describe("PrintReverse", () => {
+    it("should return an empty string if the list is empty", () => {
+      const list = new DoublyLinkedList();
+
+      const spyLogger = jest
+        .spyOn(Logger.prototype, "info")
+        .mockImplementation(() => {});
+
+      list.printReverse();
+
+      expect(spyLogger).toHaveBeenCalledWith("List is empty");
+    });
+
+    it("should print the list in reverse", () => {
+      const list = new DoublyLinkedList();
+      list.addFirst(1);
+      list.addFirst(2);
+      list.addFirst(3);
+
+      const spyLogger = jest
+        .spyOn(Logger.prototype, "info")
+        .mockImplementation(() => {});
+
+      list.printReverse();
+
+      expect(spyLogger).toHaveBeenCalledWith([1, 2, 3]);
+    });
+  });
 
   describe("add At", () => {});
   describe("add", () => {});
