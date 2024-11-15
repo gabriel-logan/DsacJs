c9602e53c98f8b4d8ea37d21be81cc0bae9b532a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-11-15 13:15:55 -0300

    feat: enhance factorial function to handle floats and add gamma function

--- src/algorithms/math/factorial.ts
+++ src/algorithms/math/factorial.ts
@@ -3,6 +3,18 @@ export default function factorial(n: number): number {
     throw new Error("Factorial of negative number is not defined");
   }
 
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  if (!Number.isInteger(n)) {
+    return factorialOfFloat(n);
+  }
+
+  return factorialOfInteger(n);
+}
+
+function factorialOfInteger(n: number): number {
   let result = 1;
 
   for (let i = 2; i <= n; i++) {
@@ -10,4 +22,34 @@ export default function factorial(n: number): number {
   }
 
   return result;
+}
+
+function factorialOfFloat(n: number): number {
+  return gamma(n + 1);
+}
+
+function gamma(n: number): number {
+  const g = 7;
+
+  if (n < 0.5) {
+    return Math.PI / (Math.sin(Math.PI * n) * gamma(1 - n));
+  }
+
+  n -= 1;
+
+  const a = [
+    0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+    771.32342877765313, -176.61502916214059, 12.507343278686905,
+    -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7,
+  ];
+
+  let x = a[0];
+
+  for (let i = 1; i < g + 2; i++) {
+    x += a[i] / (n + i);
+  }
+
+  const t = n + g + 0.5;
+
+  return Math.sqrt(2 * Math.PI) * Math.pow(t, n + 0.5) * Math.exp(-t) * x;
 }
--- tests/algorithms/math/factorial.test.ts
+++ tests/algorithms/math/factorial.test.ts
@@ -1,10 +1,28 @@
 import factorial from "../../../src/algorithms/math/factorial";
 
 describe("factorial", () => {
-  it("should return the factorial of a number", () => {
+  it("should return the factorial of 5", () => {
     const n = 5;
     const result = factorial(n);
     expect(result).toBe(120);
+  });
+
+  it("should return the factorial of 10", () => {
+    const n = 10;
+    const result = factorial(n);
+    expect(result).toBe(3628800);
+  });
+
+  it("should return the factorial of 3", () => {
+    const n = 3;
+    const result = factorial(n);
+    expect(result).toBe(6);
+  });
+
+  it("should return the factorial of PI", () => {
+    const n = Math.PI;
+    const result = factorial(n);
+    expect(result).toBeCloseTo(7.188076924);
   });
 
   it("should return 1 for 0", () => {
