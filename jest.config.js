9dba3a95d6215b34d9282348ee0f13190dba0296
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-18 12:36:13 -0300

    refactor: remove Babel configuration and migrate to Jest with TypeScript support

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import("ts-jest").JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8"
+};
